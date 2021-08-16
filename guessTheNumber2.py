552d7782c3bc12953083dbe0ad5cd592b7aecae8
James Dell XPS <jamesdradge@hotmail.com>
2021-08-16 22:56:25 +0100

    refactored to remove unnecessary numberOfGuesses variable

--- guessTheNumber2.py
+++ guessTheNumber2.py
@@ -1,22 +1,18 @@
 import sys, random
 number = random.randint(1, 20)
-numberOfGuesses = 0
 print('Im thinking of a number between 1 and 20, can you guess it?')
-for i in range(1, 7):
+for guessesTaken in range(1, 7):
     guess = input()
     if int(guess) > number:
         print('Your guess is too high.')
         print('Try again')
-        numberOfGuesses = numberOfGuesses + 1
     elif int(guess) < number:
         print('Your guess is too low.')
         print('Try again')
-        numberOfGuesses = numberOfGuesses + 1
     else:
-        numberOfGuesses = numberOfGuesses + 1
         break
 if int(guess) == number:
-    print('You guess correct! You guessed my number in ' + str(numberOfGuesses) + ' tries')
+    print('You guess correct! You guessed my number in ' + str(guessesTaken) + ' tries')
 else:
     print('Sorry, you didnt guess my number, it was ' + str(number))
 sys.exit
--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 Number guessing game following automate the boring stuff (chapter 2)
+
+my initial attempt (first commit) had an unecessary variable numberofGuesses - when I looked at the solution I realised you can just use the 'i' in the "for i in range" as a track of the number of guesses, so switched to doing that.
